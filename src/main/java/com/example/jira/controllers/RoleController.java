b013879fb6244838963951fb83a21bfc17257949
Ashish.Koli <justmailashish123@gmail.com>
2025-01-23 14:27:20 +0530

    Added update end point and delete end point

--- src/main/java/com/example/jira/controllers/RoleController.java
+++ src/main/java/com/example/jira/controllers/RoleController.java
@@ -3,6 +3,8 @@ package com.example.jira.controllers;
 import com.example.jira.models.Role;
 import com.example.jira.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,13 +17,24 @@ public class RoleController {
     RoleService roleService;
 
     @PostMapping("/create")
-    public Role createRole(@RequestBody Role role){
-        return roleService.createRole(role);
+    public ResponseEntity<Role> createRole(@RequestBody Role role){
+        return new ResponseEntity<>(roleService.createRole(role), HttpStatus.CREATED);
     }
 
     @GetMapping("/allRoles")
-    public List<Role> getAllRoles(){
-        return roleService.getAllRoles();
+    public ResponseEntity<List<Role>> getAllRoles(){
+        return new ResponseEntity<>(roleService.getAllRoles(), HttpStatus.OK) ;
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Role> updateRole(@RequestBody Role role, @PathVariable int id){
+        return new ResponseEntity<>(roleService.update(role,id),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Object> deleteRole(@PathVariable int id){
+        roleService.deleteRole(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
--- src/main/java/com/example/jira/services/RoleService.java
+++ src/main/java/com/example/jira/services/RoleService.java
@@ -17,5 +17,18 @@ public class RoleService {
         return  roleRepository.save(role);
     }
 
-    public List<Role> getAllRoles(){return roleRepository.findAll();}
+    public List<Role> getAllRoles(){
+        return roleRepository.findAll();
+    }
+
+
+    public Role update(Role role, int id) {
+        Role updateRole = roleRepository.findById(id).orElseThrow();
+        updateRole.setTitle(role.getTitle());
+        return roleRepository.save(updateRole);
+    }
+
+    public void  deleteRole(int id) {
+         roleRepository.deleteById(id);
+    }
 }
