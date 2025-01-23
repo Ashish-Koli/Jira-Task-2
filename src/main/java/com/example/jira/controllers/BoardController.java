4b70862da9c7b76d71f75d5741b7b0a00b4c9b6a
Ashish.Koli <justmailashish123@gmail.com>
2025-01-23 15:33:30 +0530

    added the getall, update and delete endpoint for board

--- src/main/java/com/example/jira/controllers/BoardController.java
+++ src/main/java/com/example/jira/controllers/BoardController.java
@@ -5,9 +5,9 @@ import com.example.jira.models.Board;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/board")
@@ -16,10 +16,25 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    @PostMapping("/create")
     public ResponseEntity<Board> createBoard(@RequestBody Board board){
         return new ResponseEntity<>(boardService.createBoard(board), HttpStatus.CREATED);
     }
 
+    @GetMapping("/allBoards")
+    public ResponseEntity<List<Board>> getAllBoards(){
+        return new ResponseEntity<>(boardService.getAllBoards(), HttpStatus.OK);
+    }
 
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Board> updateBoard(@RequestBody Board board, @PathVariable int id){
+        return new ResponseEntity<>(boardService.updateBoard(board, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteBoard(@PathVariable int id){
+        boardService.deleteBoard(id);
+        return new ResponseEntity<>("Board Deleted.",HttpStatus.NO_CONTENT);
+    }
 
 }
