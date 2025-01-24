d058627d4bca0eb70d80fca7628a5fee430cbbdd
Ashish.Koli <justmailashish123@gmail.com>
2025-01-24 12:04:26 +0530

    delete

--- src/main/java/com/example/jira/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jira.repositories;
-
-import com.example.jira.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
