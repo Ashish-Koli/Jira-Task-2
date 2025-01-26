a2675405155ef6fb8073bfab674fcd3466d68a9f
Ashish Koli <justmailashish123@gmail.com>
2025-01-26 23:25:31 +0530

    Board DTO

---
+++ src/main/java/com/example/jira/dto/BoardDTO.java
@@ -0,0 +1,29 @@
+package com.example.jira.dto;
+
+
+public class BoardDTO {
+
+    private String boardName;
+    private int project;
+
+    public BoardDTO(String boardName, int project) {
+        this.boardName = boardName;
+        this.project = project;
+    }
+
+    public String getBoardName() {
+        return boardName;
+    }
+
+    public void setBoardName(String boardName) {
+        this.boardName = boardName;
+    }
+
+    public int getProject() {
+        return project;
+    }
+
+    public void setProject(int project) {
+        this.project = project;
+    }
+}
